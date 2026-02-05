b873ed27a82effdefb2382372cc59237b520b31d
Winter-Haeum <rudnfgkdma13@gmail.com>
2026-02-05 14:39:30 +0900

    chore: update vite config for 1-2-3 github pages

--- 2026-02-04/1-2-3/vite.config.js
+++ 2026-02-04/1-2-3/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/homework-repo/1-2-3/",
   plugins: [react()],
-})
+});
