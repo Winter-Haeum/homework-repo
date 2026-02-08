44f68b7331c6c13c4c548bfad7182a8ebb5477c0
Winter-Haeum <rudnfgkdma13@gmail.com>
2026-02-08 19:07:42 +0900

    chore: fix vite base path for GitHub Pages subfolder deploy

--- 2026-02-04/1-2-3/vite.config.js
+++ 2026-02-04/1-2-3/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/homework-repo/1-2-3/",
   plugins: [react()],
+  base: "/homework-repo/1-2-3/",
 });
