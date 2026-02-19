57cd0cb87fcda79e3058af6668e76582f4fe878f
Winter-Haeum <rudnfgkdma13@gmail.com>
2026-02-20 08:14:56 +0900

    fix: github pages base 설정

--- 2026-02-19/1-3-부록-2/vite.config.js
+++ 2026-02-19/1-3-부록-2/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
   base: "/homework-repo/1-3-부록-2/",
+  plugins: [react()],
 });
